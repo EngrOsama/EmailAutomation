65b74040fbefd505a1248e969bed8e414fb36c3c
EngrOsama <97381158+EngrOsama@users.noreply.github.com>
2023-04-04 13:02:04 +0500

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from openpyxl import load_workbook
 # load list of emails from Excel file
 wb = load_workbook('email_list.xlsx')
 ws = wb.active
-emails = [cell for cell in ws['A']]
+emails = [cell for cell in ws['A'] if cell.offset(0, 1).value != 'done'] # check if neighboring cell has "done" written in it
+emails = emails[:400] # limit the number of emails to be sent to <=500
 
 # set up SMTP server and login
 smtp_server = 'smtp.gmail.com'
@@ -42,19 +43,25 @@ for email_cell in emails:
     msg.attach(MIMEText(body, 'plain'))
 
     # connect to SMTP server and send message
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.login(smtp_username, smtp_password)
-    text = msg.as_string()
-    server.sendmail(sender_email, email, text)
-    server.quit()
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        text = msg.as_string()
+        server.sendmail(sender_email, email, text)
+        server.quit()
 
-    # add delay timer after sending each email
-    time.sleep(2) # 2 seconds delay
+        # add delay timer after sending each email
+        time.sleep(0) # set delay in seconds
 
-    # write 'done' in the next column
-    done_cell = email_cell.offset(0, 1)
-    done_cell.value = 'done'
+        # write 'done' in the next column
+        done_cell = email_cell.offset(0, 1)
+        done_cell.value = 'done'
 
-    # save the file
-    wb.save('email_list.xlsx')
+        # save the file
+        wb.save('email_list.xlsx')
+        print(f"Email sent to {email}")
+
+    except smtplib.SMTPRecipientsRefused:
+        print(f"Invalid email address: {email}")
+        continue
